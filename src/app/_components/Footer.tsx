3f6984e8316a24681734d01b35d795daaf12c4d9
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-24 19:35:56 -0300

    feat: add Footer styles for sm devices or above

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -6,8 +6,8 @@ interface FooterProps {
 export function Footer({ previousStep, nextStep }: FooterProps) {
   return (
     <footer className="p-4 bg-white flex justify-between items-center">
-      <button onClick={previousStep} className="border-none text-sm text-grey font-medium">Go back</button>
-      <button onClick={nextStep} className="bg-denim py-3 px-4 rounded text-sm text-white font-medium" >Next step</button>
+      <button onClick={previousStep} className="border-none text-sm text-grey font-medium sm:text-base">Go back</button>
+      <button onClick={nextStep} className="bg-denim py-3 px-4 rounded text-sm text-white font-medium sm:text-base" >Next step</button>
     </footer>
   )
 }
