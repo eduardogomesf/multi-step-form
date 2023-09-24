6b1e6edc2dc32f40c8134b4ee22506898f0c6b84
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-24 19:38:08 -0300

    feat: start Form component with responsiveness

---
+++ src/app/_components/Form/index.tsx
@@ -0,0 +1,9 @@
+import { YourInfo } from "./steps/YourInfo";
+
+export function Form() {
+  return (
+    <div className="w-full bg-white rounded-lg px-6 py-8 mt-[calc(-1*calc(172px-96px))] sm:mt-0">
+      <YourInfo />
+    </div>
+  )
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+'use client'
+
+import { useState } from "react";
+import { Footer } from "./_components/Footer";
+import { Form } from "./_components/Form";
 import { Sidebar } from "./_components/Sidebar";
 
 const steps = [{ title: 'Your info', number: 1 }, { title: 'Select plan', number: 2 }, { title: 'ADD-ONS', number: 3 }, { title: 'Summary', number: 4 },]
 
 export default function Home() {
+  const [currentStep, setCurrentStep] = useState(1)
+
+  function handleNextStep() {
+    if (currentStep === steps.length) return
+    setCurrentStep(currentStep + 1)
+  }
+
+  function handlePreviousStep() {
+    if (currentStep === 1) return
+    setCurrentStep(currentStep - 1)
+  }
+
   return (
-    <main className="flex flex-col sm:flex-row sm:m-4 sm:mr-0">
-      <Sidebar currentStep={2} steps={steps} />
-    </main>
+    <main className={`
+      flex flex-col h-screen
+      sm:flex-row sm:m-4 sm:mr-0 sm:h-[calc(100vh-32px)]`
+    }>
+      <Sidebar currentStep={currentStep} steps={steps} />
+      <div className="flex flex-col flex-1 justify-between sm:max-w-[550px] sm:flex-0 sm:mx-auto">
+        <div className="mx-4 flex justify-center">
+          <Form />
+        </div>
+        <Footer nextStep={handleNextStep} previousStep={handlePreviousStep} />
+      </div>
+    </main >
   )
 }
