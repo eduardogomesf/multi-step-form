f0088235f7b36e458454417b4b6dfde5ebb423d5
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-24 19:32:35 -0300

    feat: add styles for devices sm or above

--- src/app/_components/Form/FormHeader.tsx
+++ src/app/_components/Form/FormHeader.tsx
@@ -5,8 +5,8 @@ interface FormHeaderProps {
 
 export function FormHeader({ title, description }: FormHeaderProps) {
   return (
-    <header className="flex flex-col gap-2">
-      <h1 className="text-denim font-bold text-2xl">{title}</h1>
+    <header className="flex flex-col gap-2 sm:gap-3">
+      <h1 className="text-denim font-bold text-2xl sm:text-3xl">{title}</h1>
       <h1 className="text-grey font-normal text-base ">{description}</h1>
     </header>
   )
