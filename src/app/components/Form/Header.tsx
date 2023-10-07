ae34d5e18086a933c754c77b1f7a9dea4525f03f
Eduardo Gomes <edugomesf@icloud.com>
2023-10-07 13:15:01 -0300

    fix: change step description to p tag

--- src/app/components/Form/Header.tsx
+++ src/app/components/Form/Header.tsx
@@ -7,7 +7,7 @@ export function Header({ title, description }: HeaderProps) {
   return (
     <header className="flex flex-col gap-2 sm:gap-3">
       <h1 className="text-denim font-bold text-2xl sm:text-3xl">{title}</h1>
-      <h1 className="text-grey font-normal text-base ">{description}</h1>
+      <p className="text-grey font-normal text-base ">{description}</p>
     </header>
   )
 } 
